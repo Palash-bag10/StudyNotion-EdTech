fe4f26672ed1b2029f09de7611649ef460e7c90f
Palash Bag <palashbag903@gmail.com>
2023-08-18 01:59:47 +0530

    feature/add-styles-VerifyEmail.jsx page

--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -37,16 +37,16 @@ const VerifyEmail = () => {
     }
 
   return (
-    <div className='text-white'>
+    <div className=' min-h-[calc(100vh-3.5rem)] grid place-items-center'>
       {
         loading 
         ? ( <div className='spinner'></div> )
         : (
-           <div>
+           <div className=' max-w-[500px] p-4 lg:p-8 gap-6'>
 
-            <h3>Verify email</h3>
+            <h3 className=' text-richblack-5 font-semibold text-3xl'>Verify email</h3>
 
-            <p>A verification code has been sent to you. Enter the code below</p>
+            <p className=' text-lg font-normal text-richblack-100 mt-3'>A verification code has been sent to you. Enter the code below</p>
 
             <form onSubmit={handleOnSubmit}>
                 <OTPInput 
@@ -54,30 +54,43 @@ const VerifyEmail = () => {
                     onChange={setOtp}
                     numInputs={6}
                     renderSeparator={<span>-</span>}
-                    renderInput={(props) => (<input {...props} 
-                        className='text-black'
+                    renderInput={(props) => (
+                        <input 
+                            {...props} 
+                            style={{
+                                boxShadow: "inset 0px -1px 0px rgba(255, 255, 255, 0.18)",
+                            }}
+                        className='w-[48px] lg:w-[60px] border-0 bg-richblack-800 rounded-md aspect-square text-richblack-5 text-center focus:border-0 focus:outline-2 focus:outline-yellow-50 '
                     />)}
-                    
+                    containerStyle={{
+                    justifyContent: "space-between",
+                    marginTop: "24px", marginBottom: "24px"
+                    }}
                 />
 
-                <button type='submit'>
+                <button
+                 type='submit'
+                 className=' w-full bg-yellow-50 p-3 rounded-lg font-semibold text-richblack-900 text-base'
+                 >
                     Verify Email
                 </button>
             </form>
 
-            <div>
-
-                <div>
-                    <Link to="/login">
-                        <BsArrowLeftShort/>
+            <div className=' flex items-center justify-between mt-3'>
+                <div className=' '>
+                    <Link 
+                    to="/login"
+                    className='flex text-richblack-5 items-center gap-x-2 text-base font-medium'>
+                        <BsArrowLeftShort size={30}/>
                         <p>Back to Login</p>
                     </Link>
                 </div>
 
                 <button
-                    onClick={() => dispatch(sendotp(signupData.email, navigate))}    
+                    onClick={() => dispatch(sendotp(signupData.email, navigate))}
+                    className='flex text-blue-100 items-center gap-x-2 text-base font-medium'    
                 >
-                    <BiRotateLeft/>
+                    <BiRotateLeft size={30}/>
                     <p>Resend</p>
                 </button>
 
