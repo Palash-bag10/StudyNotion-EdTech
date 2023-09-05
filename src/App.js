a6484acf7ecde3bebf8d83a80f29bdf014ee25e1
Palash Bag <palashbag903@gmail.com>
2023-09-05 16:14:42 +0530

    [FEAT]: added Enrolled Courses Page and Render Course Card

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import MyProfile from "./components/core/Dashboard/MyProfile";
 import Dashboard from "./pages/Dashboard";
 import Error from "./pages/Error";
 import Settings from "./components/core/Dashboard/Settings";
+import EnrollCourses from "./components/core/Dashboard/EnrollCourse/EnrollCourses";
 
 function App() {
 
@@ -81,6 +82,7 @@ function App() {
         >
           <Route path="/dashboard/my-profile" element={<MyProfile/>}/>
           <Route path="/dashboard/settings" element={<Settings/>}/>
+          <Route path="/dashboard/enrolled-courses" element={<EnrollCourses/>}/>
 
 
         </Route>
--- src/components/core/Dashboard/EnrollCourse/EnrollCourses.jsx
+++ src/components/core/Dashboard/EnrollCourse/EnrollCourses.jsx
@@ -1,8 +1,78 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { getUserEnrolledCourse } from '../../../../services/operation/ProfileAPI';
+import ProgressBar from '@ramonak/react-progress-bar';
 
 const EnrollCourses = () => {
+
+  const {token} = useSelector((state) => state.auth);
+
+  const [enrolledCourses, setEnrolledCourses] = useState(null);
+
+  const getEnrolledCourses = async() => {
+    try{
+
+      const response = getUserEnrolledCourse(token);
+      setEnrolledCourses(response);
+
+    } catch(error){
+      console.log('Unable to fetch enrolled courses')
+    }
+  }
+
+  useEffect(() => {
+    getEnrolledCourses();
+  },[])
+
   return (
-    <div>
+    <div className=' text-white'>
+
+      <h2>Enrolled Courses</h2>
+
+      {
+        !enrolledCourses 
+        ? ( <div className='spinner'></div> )
+        : !enrolledCourses.length 
+        ? ( <p>You don't have any enrolled courses</p> )
+        : ( <div>
+              <div>
+                <p>Course Name</p>
+                <p>Duration</p>
+                <p>Progress</p>
+              </div>
+
+              {/* CARD */}
+              {
+                enrolledCourses.map((course, index) => (
+                  <div>
+                    <div>
+                      <img src={course.thumbnail} />
+                      <div>
+                        <p>{course.courseName}</p>
+                        <p>{course.courseDescription}</p>
+                      </div>
+                    </div>
+
+                    <div>
+                      {course?.totalDuration}
+                    </div>
+
+                    <div>
+                      <p>Progress: {course.progressPercentage || 0}%</p>
+                      <ProgressBar
+                        completed={course.progressPercentage || 0}
+                        height='8px'
+                        isLabelVisible={false}
+                      />
+                    </div>
+
+                  </div>
+                ))
+              }
+
+            </div> 
+          )
+      }
       
     </div>
   )
