e15903dfc193747d0e6ba04cb5ec95b38cec4b63
Palash Bag <palashbag903@gmail.com>
2023-09-13 12:22:58 +0530

    [FEAT]: added Cart Informatiom Feature

--- src/components/core/Dashboard/Cart/index.jsx
+++ src/components/core/Dashboard/Cart/index.jsx
@@ -1,12 +1,28 @@
+import { useSelector } from "react-redux"
 
 
 export default function Cart() {
 
+    const {totalPrice, totalItems} = useSelector((state) => state.cart)
 
     return (
         <div>
+            <h2>Your Cart</h2>
+            <p>{totalItems} Courses in Cart</p>
 
+            {
+                totalPrice > 0
+                ? (
+                    <div>
+                        <RenderCartCourses/>
+                        <RenderTotalAmount/>
+                    </div>
+                  )
+                : (
+                    <p>Your Cart is Empty</p>
+                  )
+            }
         </div>
     )
-    
+
 }
