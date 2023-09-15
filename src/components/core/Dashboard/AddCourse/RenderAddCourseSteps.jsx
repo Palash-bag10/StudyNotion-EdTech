339442a4d4558c20e3017acc5279c10a548d123c
Palash Bag <palashbag903@gmail.com>
2023-09-15 22:10:52 +0530

    added Add Course Steps

--- src/components/core/Dashboard/AddCourse/RenderAddCourseSteps.jsx
+++ src/components/core/Dashboard/AddCourse/RenderAddCourseSteps.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
+import {FaCheck} from "react-icons/fa"
 
 const RenderAddCourseSteps = () => {
+
+  const steps = [
+    {
+      id: 1,
+      title: "Course Information",
+    },
+    {
+      id: 2,
+      title: "Course Builder",
+    },
+    {
+      id: 3,
+      title: "Publish",
+    },
+  ]
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <div>
+        {
+          steps.map((item) => (
+            <div 
+            className={`${step === item.id 
+                      ? " bg-yellow-900 text-yellow-50 border-yellow-50"
+                      : " border-richblack-700 bg-richblack-800 text-richblack-300"}`}
+            >
+              {step > item.id ? (<FaCheck/>) : (item.id)}
+            </div>
+          ))
+        }
+      </div>
+    </>
   )
 }
 
