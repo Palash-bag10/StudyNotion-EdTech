e6993fa41e8847ea4c7e722bcf70815da6b2f700
Palash Bag <palashbag903@gmail.com>
2023-09-27 13:09:25 +0530

    added some condition for rander image and video

--- src/components/core/Dashboard/AddCourse/MediaUpload.jsx
+++ src/components/core/Dashboard/AddCourse/MediaUpload.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { useRef } from 'react'
 import { useDropzone } from 'react-dropzone'
 import {FiUpload} from 'react-icons/fi'
+import { Player } from 'video-react'
 
 const MediaUpload = ({
     name,
@@ -15,7 +16,9 @@ const MediaUpload = ({
 }) => {
 
     const [selectedFile, setSelectedFile] = useState(null)
-    const [previewSource, setPreviewSource] = useState(viewData)
+    const [previewSource, setPreviewSource] = useState(
+      viewData ? viewData : editData ? editData : ""
+    )
 
     const inputRef = useRef(null)
 
@@ -35,7 +38,7 @@ const MediaUpload = ({
       }
     }
 
-    const {getRootProps, getInputProps, isDragAccept} = useDropzone({
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({
       accept: !video ? {"image" : [".jpeg", ".jpg", ".png"]} : {"video" : [".mp4"]},
       onDrop,
     })
@@ -46,22 +49,49 @@ const MediaUpload = ({
     <div>
       <label htmlFor={name}> {lable}<sup className=' text-pink-300'>*</sup> </label>
 
-      <div>
-        {/* {previewSource && ( */}
+      <div className={`${isDragActive ? "bg-richblack-600" : " bg-richblack-700"} flex items-center justify-center border-dotted rounded-lg min-h-[250px] cursor-pointer border-2 border-richblack-500`}>
+        {previewSource ?
+          (
+              <div>
+                {!video 
+                ? (
+                  <img
+                   src={previewSource}
+                    alt="Preview"
+                    className=' h-full w-full rounded-md object-cover'
+                     />
+                )
+                : (
+                  <Player aspectRatio='16:9' playsInline src={previewSource}/>
+                )}
+                {
+                  !viewData && (
+                    <button
+                    type='button'
+                    onClick={() => {
+                      setPreviewSource("")
+                      setSelectedFile(null)
+                      setValue(name, null)
+                    }}
+                    >Cancle</button>
+                  )
+                }
+              </div>
+          ) : ( 
             <div>
-                <input ref={inputRef}/>
-                <div>
-                    <FiUpload/>
-                </div>
-                <p>
-                    Drag and drop an image, or <span className=' text-yellow-200'>Browse</span>  Max 6MB each (12MB for videos)
-                </p>
-                <ul>
-                    <li>Aspect ratio 16:9</li>
-                    <li>Recommended size 1024x576</li>
-                </ul>
-            </div>
-        {/* )} */}
+                  <input ref={inputRef}/>
+                  <div>
+                      <FiUpload/>
+                  </div>
+                  <p>
+                      Drag and drop an image, or <span className=' text-yellow-200'>Browse</span>  Max 6MB each (12MB for videos)
+                  </p>
+                  <ul>
+                      <li>Aspect ratio 16:9</li>
+                      <li>Recommended size 1024x576</li>
+                  </ul>
+              </div>
+           )}
       </div>
 
       {errors[name] && (
