e3f228f8db117332c519e71a613098a29aea07e1
Palash Bag <palashbag903@gmail.com>
2023-10-05 21:32:10 +0530

    connect frontend to backend for Fetching all courses under specific instructor

--- src/services/operation/courseDetailsAPI.js
+++ src/services/operation/courseDetailsAPI.js
@@ -6,6 +6,7 @@ const{
     COURSE_CATEGORIES_API,
     CREATE_COURSE_API,
     EDIT_COURSE_API,
+    GET_ALL_INSTRUCTOR_COURSES_API,
 } = courseEndPoints
 
 
@@ -66,6 +67,32 @@ export const editCourseDetails = async (data, token) => {
         result = response?.data?.data
     }catch(error){
         console.log("EDIT COURSE API ERROR...", error)
+        toast.error(error.message)
+    }
+    toast.dismiss(toastId)
+    return result
+}
+
+// Fetching all courses under specific instructor
+export const fetchInstructorCourses = async (token) => {
+    let result = [];
+    const toastId = toast.loading("Loading...")
+    try{
+        const response = await apiConnector(
+            "GET",
+            GET_ALL_INSTRUCTOR_COURSES_API,
+            null,
+            {
+                Authorization: `Bearer ${token}`,
+            }
+        )
+        console.log("INSTRUCTOR COURSES RESPONSE...", response)
+        if(!response?.data?.success){
+            throw new Error("Could not fetch Instructor Courses")
+        }
+        result = response?.data?.data
+    }catch(error){
+        console.log("INSTRUCTOR COURSES ERROR...", error)
         toast.error(error.message)
     }
     toast.dismiss(toastId)
