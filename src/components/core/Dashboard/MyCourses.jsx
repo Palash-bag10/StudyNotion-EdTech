8afab74f9778ddfa0ec166a377559c4cf00458c9
Palash Bag <palashbag903@gmail.com>
2023-10-05 20:46:27 +0530

    [FEAT]: added page heading and add new course button in MyCourses component

--- src/components/core/Dashboard/MyCourses.jsx
+++ src/components/core/Dashboard/MyCourses.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import IconButton from '../../common/IconButton'
+import {AiOutlinePlusCircle} from "react-icons/ai"
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const MyCourses = () => {
+
+    
+    const navigate = useNavigate();
+
   return (
-    <div>
-      
+    <div className=' text-white'>
+      <div>
+        <h2>My Courses</h2>
+        <IconButton 
+            text="New"
+            onclick={() => navigate("/dashboard/add-course") }
+        > <AiOutlinePlusCircle/> </IconButton>
+      </div>
     </div>
   )
 }
