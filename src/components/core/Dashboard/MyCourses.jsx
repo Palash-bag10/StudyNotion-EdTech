5dcefcffaeb4b4bca1812d803b13d46911c77669
Palash Bag <palashbag903@gmail.com>
2023-10-05 22:12:44 +0530

    [FEAT]: fetching all courses

--- src/components/core/Dashboard/MyCourses.jsx
+++ src/components/core/Dashboard/MyCourses.jsx
@@ -3,11 +3,25 @@ import IconButton from '../../common/IconButton'
 import {AiOutlinePlusCircle} from "react-icons/ai"
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { fetchInstructorCourses } from '../../../services/operation/courseDetailsAPI'
+import { useState } from 'react'
 
 const MyCourses = () => {
 
-    
+    const {token} = useSelector((state) => state.auth)
     const navigate = useNavigate();
+    const [courses, setCourses] = useState([]);
+
+    useEffect(() => {
+        const fetchCourses = async () => {
+            const result = await fetchInstructorCourses(token)
+            if(result){
+                setCourses(result)
+            }
+        }
+        fetchCourses();
+    },[])
 
   return (
     <div className=' text-white'>
