7c22417b40d5300c3a90d39bc2b3730c0e63ca67
Palash Bag <palashbag903@gmail.com>
2023-10-31 01:22:32 +0530

    [FEAt]: added button for create section

--- src/components/core/Dashboard/AddCourse/CourseBuilder/CourseBuilderForm.jsx
+++ src/components/core/Dashboard/AddCourse/CourseBuilder/CourseBuilderForm.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
+import IconButton from '../../../../common/IconButton';
+import {AiOutlinePlusCircle} from "react-icons/ai"
 
 const CourseBuilderForm = () => {
+
+  const [editSectionName, setEditSectionName] = useState(null)
 
     const {
         register,
@@ -14,7 +18,7 @@ const CourseBuilderForm = () => {
     <div>
       <p>Course Builder</p>
       <form>
-        <div>
+        <div> 
           <label htmlFor="sectionName">
             Section Name<sup>*</sup>
           </label>
@@ -28,6 +32,15 @@ const CourseBuilderForm = () => {
           {errors.sectionName && (
             <span className="ml-2 text-xs tracking-wide text-pink-200">Section Name is required</span>
           )}
+        </div>
+
+        <div className='mt-5'>
+          <IconButton
+            type="submit" 
+            text={editSectionName ? "Edit Section Name" : "Create Section"}
+          >
+            <AiOutlinePlusCircle/>
+          </IconButton>
         </div>
       </form> 
     </div>
