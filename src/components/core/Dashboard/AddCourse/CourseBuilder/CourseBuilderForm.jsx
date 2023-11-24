61443fd98f92d8722271a6b04087eafbb7be5083
Palash Bag <palashbag903@gmail.com>
2023-11-24 17:08:09 +0530

    [FIX]: fix the issue of gotoNext step 3, change subSection spelling

--- src/components/core/Dashboard/AddCourse/CourseBuilder/CourseBuilderForm.jsx
+++ src/components/core/Dashboard/AddCourse/CourseBuilder/CourseBuilderForm.jsx
@@ -67,7 +67,7 @@ const CourseBuilderForm = () => {
         toast.error("Please add atleast One Section");
         return;
       }
-      if(course.courseContent.some((section) => section.SubSection.length === 0)) {
+      if(course.courseContent.some((section) => section.subSection.length === 0)) {
         toast.error("Please Add atleast one lecture in each section");
         return;
       }
