a89357320ffd1e74b408659fa092b9e34e98b026
Palash Bag <palashbag903@gmail.com>
2023-11-24 20:26:45 +0530

    [FEAT]: added Publish Course component for final step to publish our course

--- src/components/core/Dashboard/AddCourse/RenderAddCourseSteps.jsx
+++ src/components/core/Dashboard/AddCourse/RenderAddCourseSteps.jsx
@@ -3,6 +3,7 @@ import {FaCheck} from "react-icons/fa"
 import { useSelector } from 'react-redux'
 import CourseInformationForm from './CourseInformation/CourseInformationForm'
 import CourseBuilderForm from './CourseBuilder/CourseBuilderForm'
+import PublishCourse from './PublishCourse'
 
 const RenderAddCoursesteps = () => {
 
@@ -76,7 +77,7 @@ const RenderAddCoursesteps = () => {
 
       {step === 1 && <CourseInformationForm/>}
       {step === 2 && <CourseBuilderForm />}
-      {/* {step === 3 && <PublishCourse />} */}
+      {step === 3 && <PublishCourse />}
     </>
   )
 }
