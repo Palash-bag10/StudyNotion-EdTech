62f7d0a67f8094553d812a9c10135bb5d34908b3
Palash Bag <palashbag903@gmail.com>
2023-11-24 21:20:11 +0530

    [FEAT]: added checkbox for publish the course in publishCourse.jsx

--- src/components/core/Dashboard/AddCourse/PublishCourse/index.js
+++ src/components/core/Dashboard/AddCourse/PublishCourse/index.js
@@ -1,11 +1,45 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
 
 
 
 export default function PublishCourse(){
 
+    const {
+        register,
+        handleSubmit,
+        setValue,
+        getValues,
+        formState: {errors},
+    } = useForm();
+
+    const {course} = useSelector((state) => state.course);
+    const {token} = useSelector((state) => state.auth);
+    const dispatch = useDispatch();
+    const [loading, setLoading] = useState(false);
+
+    const onSubmit = () => {
+
+    }
+
     return(
-        <div>
-            
+        <div className=" bg-richblack-800 border-richblack-700 p-6 rounded-md border-[1px]">
+         <p className=" text-richblack-5 font-semibold text-2xl">Publish Course</p>
+         <form onSubmit={handleSubmit(onSubmit)}>
+            <div className=" my-6 mb-8">
+              <label htmlFor="public" className=" items-center inline-flex text-medium">
+                <input 
+                    type="checkbox" 
+                    id="public"
+                    {...register("public", {required: true})}
+                    className=" h-4 w-4 bg-richblack-500 border-gray-300 focus:ring-2 focus:ring-richblack-5 rounded" 
+                    />
+
+                    <span className=" ml-2 text-richblack-400">Make this Course Public</span>
+              </label>
+            </div>
+         </form>
         </div>
     );
 }
