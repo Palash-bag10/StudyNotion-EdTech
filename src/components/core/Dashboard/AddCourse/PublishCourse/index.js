610d4cb335c8d2b04bbb74c56d33b12018a8fe52
Palash Bag <palashbag903@gmail.com>
2023-11-25 00:28:53 +0530

    [FEAT]: write down the logic of onSubmit function when publish the course

--- src/components/core/Dashboard/AddCourse/PublishCourse/index.js
+++ src/components/core/Dashboard/AddCourse/PublishCourse/index.js
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
 import IconButton from "../../../../common/IconButton";
+import { resetCourseState, setStep } from "../../../../../slices/courseSlice";
+import { COURSE_STATUS } from "../../../../../utils/constants";
+import { editCourseDetails } from "../../../../../services/operation/courseDetailsAPI";
 
 
 
@@ -20,12 +23,46 @@ export default function PublishCourse(){
     const dispatch = useDispatch();
     const [loading, setLoading] = useState(false);
 
-    const onSubmit = () => {
+    useEffect(() => {
+        if(course?.status === COURSE_STATUS.PUBLISHED){
+            setValue("public", true);
+        }
+    },[])
 
+    const onSubmit = () => {
+        handleCoursePublish(); 
     }
 
-    const goBack = () => {
+    const handleCoursePublish = async() => {
+        if(course?.status === COURSE_STATUS.PUBLISHED && getValues("public") === true ||
+        (course?.status === COURSE_STATUS.DRAFT && getValues("public") === false)){
+            // no updation in form
+            // no need to make api call
+            goToCourses();
+            return;
+        }
 
+        // if form is updated
+        const formData = new FormData();
+        formData.append("courseId", course._id);
+        const courseStatus = getValues("public") ? COURSE_STATUS.PUBLISHED : COURSE_STATUS.DRAFT;
+        formData.append("status", courseStatus);
+
+        setLoading(true);
+        const result = await editCourseDetails(formData, token);
+        if(result){
+            goToCourses();
+        }
+        setLoading(false);
+    }
+
+    const goToCourses = () => {
+        dispatch(resetCourseState());
+        // navigate("/dashboard/my-courses")
+    }
+ 
+    const goBack = () => {
+        dispatch(setStep(2))
     }
 
     return(
