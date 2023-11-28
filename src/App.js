54eba8da1f1399f64c29cb0c83eb8cb323ee9a3b
Palash Bag <palashbag903@gmail.com>
2023-11-29 01:32:17 +0530

    add EditCourse route in app.js

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ import { ACCOUNT_TYPE } from "./utils/constants";
 import { useSelector } from "react-redux";
 import AddCourse from "./components/core/Dashboard/AddCourse";
 import MyCourses from "./components/core/Dashboard/MyCourses";
+import EditCourse from "./components/core/Dashboard/EditCourse";
 
 function App() {
 
@@ -104,7 +105,7 @@ function App() {
               <>
               <Route path="/dashboard/add-course" element={<AddCourse/>}/>
               <Route path="/dashboard/my-courses" element={<MyCourses/>}/>
-              {/* <Route path="/dashboard/edit-course/:courseId" element={<EditCourses/>}/> */}
+              <Route path="/dashboard/edit-course/:courseId" element={<EditCourse/>}/>
               </>
             )
           }
