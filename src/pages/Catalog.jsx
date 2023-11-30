4b4cfe6ef9b279e328ab4b02d1fbcf8fe841cc0a
Palash Bag <palashbag903@gmail.com>
2023-11-30 20:40:39 +0530

    write the logic of fetch all categories in catalog component

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom'
+import { apiConnector } from '../services/apiconnector';
+import { categories } from '../services/apis';
+import { getCatalogPageData } from '../services/operation/pageAndComponentData';
+
+
 
 const Catalog = () => {
+
+    const {catalogName} = useParams();
+    const [catalogPageData, setCatalogPageData] = useState(null);
+    const [categoryId, setCategoryId] = useState("");
+
+    // fetch all categories
+    useEffect(() => {
+        const getCategories = async() => {
+            const res = await apiConnector("GET", categories.CATEGORIES_API);
+            const category_id = res?.data?.data?.filter((category) => category.name.split(" ").join("-").toLowerCase() === catalogName)[0]._id;
+            setCategoryId(category_id);
+        }
+        getCategories();
+    },[catalogName])
+
+    useEffect(() => {
+        const getCategoryDetails = async() => {
+            try{
+                const res = await getCatalogPageData(categoryId);
+                setCatalogPageData(res);
+            } catch(error){
+                console.log(error)
+            }
+        }
+        getCategoryDetails();
+    },[categoryId])
+
   return (
     <div className=' text-white'>
-      We are in Catalog Page
+      
     </div>
   )
 }
