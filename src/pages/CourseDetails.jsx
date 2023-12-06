c98a90f24ab9060c8faa76d945fb612e3fad8692
Palash Bag <palashbag903@gmail.com>
2023-12-06 16:30:50 +0530

    [FEAT]: ceate a Buy Now button to buy the course

--- src/pages/CourseDetails.jsx
+++ src/pages/CourseDetails.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 const CourseDetails = () => {
+ 
+    const {token} = useSelector((state) => state.auth)
+    
+    const handleBuyCourse = () => {
+        if(token){
+            buyCourse();
+            return;
+        }
+    }
+
+
   return (
-    <div>
-      
+    <div className=' flex items-center justify-center'>
+      <button 
+      onClick={() => handleBuyCourse()}
+      className=' flex bg-yellow-5 p-4 mt-16'>
+        Buy Now
+      </button>
     </div>
   )
 }
