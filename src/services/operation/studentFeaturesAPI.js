726d9217a002b46fe38dacded1c79ed1d0463798
Palash Bag <palashbag903@gmail.com>
2023-12-06 17:23:22 +0530

    create a Load Script function

---
+++ src/services/operation/studentFeaturesAPI.js
@@ -0,0 +1,23 @@
+const { studentEndpoints } = require("../apis");
+
+
+const {
+    COURSE_PAYMENT_API,
+    COURSE_VERIFY_API,
+    SEND_PAYMENT_SUCCESS_EMAIL_API,
+} = studentEndpoints;
+
+function loadScript(src) {
+    return new Promise((resolve) => {
+        const script = document.createElement("script");
+        script.src = src;
+
+        script.onload = () => {
+            resolve(true)
+        }
+        script.onerror = () => {
+            resolve(false)
+        }
+        document.body.appendChild(script)
+    })
+}
