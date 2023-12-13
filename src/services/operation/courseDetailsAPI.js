84c3df230758bd934401f8d97ea5741f3273ffa7
Palash Bag <palashbag903@gmail.com>
2023-12-13 22:33:55 +0530

    connect completed lecture api with frontend to backend

--- src/services/operation/courseDetailsAPI.js
+++ src/services/operation/courseDetailsAPI.js
@@ -18,6 +18,7 @@ const{
     GET_ALL_COURSE_API,
     GET_FULL_COURSE_DETAILS_AUTHENTICATED,
     CREATE_RATING_API,
+    LECTURE_COMPLETION_API,
 } = courseEndPoints
 
 
@@ -343,4 +344,29 @@ export const createRating = async(data, token) => {
     }
     toast.dismiss(toastId)
     return success
+}
+
+// mark a lecture as complete
+export const markLectureAsComplete = async(data, token) => {
+    let result = null
+    console.log("MARK COMPLETE DATA", data)
+    const toastId = toast.loading("Loading...")
+    try{
+        const response = await apiConnector("POST", LECTURE_COMPLETION_API, data, {
+            Authorization: `Bearer ${token}`,
+        })
+        console.log("MARK_LECTURE_AS_COMPLETE_API RESPONSE...", response)
+
+        if(!response.data.message){
+            throw new Error(response.data.error)
+        }
+        toast.success("Lecture Completed")
+        result = true
+    } catch(error) {
+        console.log("MARK_LECTURE_AS_COMPLETE_API API ERROR....", error)
+        toast.error(error.message)
+        result = false
+    }
+    toast.dismiss(toastId)
+    return result
 }
