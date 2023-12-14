574cb335db769ea47c12590db930b35fccb94c41
Palash Bag <palashbag903@gmail.com>
2023-12-14 12:21:23 +0530

    write the logic of video render when page loading in the first time

--- src/components/core/ViewCourse/VideoDetais.jsx
+++ src/components/core/ViewCourse/VideoDetais.jsx
@@ -1,6 +1,66 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { useLocation, useNavigate, useParams } from 'react-router-dom'
 
 const VideoDetais = () => {
+
+  const {courseId, sectionId, subSectionId} = useParams();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const playerRef = useRef();
+  const {token} = useSelector((state) => state.auth);
+  const {courseSectionData, courseEntireData, completedLectures} = useSelector((state) => state.viewCourse);
+
+  const [videoData, setVideoData] = useState([]);
+  const [videoEnded, setVideoEnded] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const setVideoSpacificData = async() => {
+      // check if data is present
+      if(!courseSectionData.length) return;
+
+      // if there is no data
+      if(!courseId && !sectionId && !subSectionId){
+        // navigate enrolled courses page
+        navigate("/dashboard/enrolled-courses");
+      }
+      else{
+        // if data present
+        const filterData = courseSectionData.filter(
+          (course) => course._id === sectionId
+        )
+
+        const filterVideoData = filterData?.[0].subsection.filter((data) => data._id === subSectionId)
+
+        setVideoData(filterVideoData[0]);
+        setVideoEnded(false)
+      }
+    }
+    setVideoSpacificData();
+  }, [courseSectionData, courseEntireData, location.pathname])
+
+  const isFirstVideo = () => {
+    
+  }
+
+  const isLastVideo = () => {
+
+  }
+
+  const goToNextVideo = () => {
+
+  }
+
+  const goToPrevVideo = () => {
+
+  }
+
+  const handleLectureCompletion = () => {
+
+  }
+
   return (
     <div>
       
