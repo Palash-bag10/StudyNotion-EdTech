2d0c1ab7f614cee0a847e1d11037c751878feb16
Palash Bag <palashbag903@gmail.com>
2023-12-18 19:42:31 +0530

    fetch getInstructorData api in useEffect to get all instructor data

--- src/components/core/Dashboard/InstructorDashboard/Instructor.jsx
+++ src/components/core/Dashboard/InstructorDashboard/Instructor.jsx
@@ -1,6 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { getInstructorData } from '../../../../services/operation/ProfileAPI'
+import { fetchInstructorCourses } from '../../../../services/operation/courseDetailsAPI'
 
 const Instructor = () => {
+
+    const {token} = useSelector((state) => state.auth)
+    const {user} = useSelector((state) => state.profile)
+    const [loading, setLoading] = useState(false)
+    const [instructorData, setInstructorData] = useState(null)
+    const [courses, setCourses] = useState([])
+
+    useEffect(() => {
+
+        const getCourseDataWithStats = async() => {
+            setLoading(true)
+            const instructorApiData = await getInstructorData(token)
+            const result = await fetchInstructorCourses(token)
+
+            console.log(instructorApiData)
+
+            if(instructorApiData.length){
+                setInstructorData(instructorApiData)
+            }
+            if(result){
+                setCourses(result)
+            }
+            setLoading(false)
+        }
+        getCourseDataWithStats()
+
+    },[])
+
   return (
     <div>
       
