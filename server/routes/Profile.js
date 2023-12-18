7b34e0b96312190451af80eb33593c95cfa4394d
Palash Bag <palashbag903@gmail.com>
2023-12-18 12:45:01 +0530

    added routes for instructor dashboard

--- server/routes/Profile.js
+++ server/routes/Profile.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 // IMPORT CONTROLLERS
-const {updateProfile, deleteAccount, getAllUserDetails, updateDisplayPictue, getEnrolledCourse} = require("../controllers/Profile");
-const {auth} = require("../middlewares/auth");
+const {updateProfile, deleteAccount, getAllUserDetails, updateDisplayPictue, getEnrolledCourse, instructorDashboard} = require("../controllers/Profile");
+const {auth, isInstructor} = require("../middlewares/auth");
 
 // DEFINE API ROUTES
 router.put("/updateprofile", auth, updateProfile);
@@ -11,5 +11,6 @@ router.delete("/deleteaccount", auth, deleteAccount);
 router.get("/getalluserdetails", auth, getAllUserDetails);
 router.put("/updateDisplayPictue", auth, updateDisplayPictue);
 router.get("/getEnrolledCourse", auth, getEnrolledCourse);
+router.get("/instructorDashboard", auth, isInstructor , instructorDashboard);
 
 module.exports = router;
