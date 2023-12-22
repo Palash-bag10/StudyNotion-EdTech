ae4a29297852786a65022b2c9038e530919b7da9
Palash Bag <palashbag903@gmail.com>
2023-12-22 19:37:41 +0530

    styled IconButton Component

--- src/components/common/IconButton.jsx
+++ src/components/common/IconButton.jsx
@@ -14,13 +14,13 @@ const IconButton = ({
     disabled={disabled}
     onClick={onclick}
     type={type}
-    className={`flex items-center ${ outline ? "border border-yellow-50 bg-transparent" : " bg-yellow-50"} cursor-pointer gap-x-2 rounded-md px-5 py-2 font-semibold text-richblack-900`}
+    className={`flex items-center ${ outline ? "border border-yellow-50 bg-transparent" : " bg-yellow-50"} cursor-pointer gap-x-2 rounded-md px-5 py-2 font-semibold text-richblack-900 ${customClasses}`}
     >
       {
         children 
         ? (
             <>
-                <span> {text} </span>
+                <span className={`${outline && "text-yellow-50"}`}> {text} </span>
                 {children}
             </>
         )
